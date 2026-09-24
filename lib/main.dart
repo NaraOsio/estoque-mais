dae142c58bc1dce66b8c3cffdcd6d1b7061fee6a
Projeto Carro Autonomo <osiosilvane@gmail.com>
2026-09-24 00:42:24 -0300

    Configura Firebase no aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'features/produtos/view/lista_produtos_page.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const EstoqueMaisApp());
 }
 
